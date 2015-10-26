17744dc421abaa698a661682b0013ff551f54ed8
Joel Elizaga <joelizaga@hotmail.com>
2015-10-26 09:54:58 -0700

    Semi-finished.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,48 @@
-/* your code goes here! */
+/*
+// Select element from the DOM
+$('selector');
 
-/* Use clever selections and event handlers to assign the described interactivity */
+// Assign event
+$('selector').on('specify-event', function(){});
 
+// Create HTML element
+var li = $('<li>');
+
+// Add to page
+$('ol').append(li);
+
+// Or
+li.appendTo($('ol'));
+*/
+
+$("button").on("click", function() {
+
+    var thisButton = this.id;
+    var selectorToModify = $("#selector").val();
+    var contentToApply = $("#newContent").val();
+
+    if (thisButton == "change-content") {
+        $(selectorToModify).val(contentToApply);
+    } else if (thisButton == "add-at-end") {
+        $(selectorToModify).append(contentToApply);
+    } else if (thisButton == "add-at-start") {
+        $(selectorToModify).prepend(contentToApply);
+    } else if (thisButton == "insert-before") {
+        $(selectorToModify).before(contentToApply);
+    } else if (thisButton == "move-after") {
+        $(selectorToModify).after(contentToApply);
+    } else if (thisButton == "surround-class") {
+        $(selectorToModify).wrap("<div class='" + contentToApply + "'></div>");
+    } else if (thisButton == "hide-text") {
+        if ($(selectorToModify).length() > 12) { 
+            $(selectorToModify).wrap("<div class='hidden'></div>");
+        }
+    } else if (thisButton == "remove-word") {
+        if ($(selectorToModify).has(contentToApply)) {
+            $(selectorToModify).wrap("<div class='hidden'></div>");
+        }
+    } else {
+        alert("What button did you press??");
+    }
+
+});
